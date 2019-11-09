f68b199d86506ae29ec2d4bc977810c2a766db79
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-11-09 18:30:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,5 +6,17 @@ class School
   end 
   
   def add_student (student, grade)
-    
+    if !@roster.has_key?(grade)
+            @roster[grade] = []
+        end
+        @roster[grade] << student
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    @roster.each {|grade, students| students.sort!}
+  end
 end 
